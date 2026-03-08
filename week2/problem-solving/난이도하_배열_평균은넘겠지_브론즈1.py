3d787da8764425f7f348af469f4140be8d41b74f
IMGyuGo <gyugo4894@gmail.com>
2026-03-08 23:06:47 +0900

    난이도하푸는중...

--- week2/problem-solving/난이도하_배열_평균은넘겠지_브론즈1.py
+++ week2/problem-solving/난이도하_배열_평균은넘겠지_브론즈1.py
@@ -1,2 +1,17 @@
 # 배열 - 평균은 넘겠지 (백준 브론즈1)
 # 문제 링크: https://www.acmicpc.net/problem/4344
+
+C = int(input())
+for _ in range(C) :
+  nums = list(map(int, input().split()))
+  n = nums[0]
+  scores = nums[1:]
+
+  mean = sum(scores)/n
+  up = 0
+  for score in scores :
+    if mean < score :
+      up+=1
+  
+  percent = up / float(n) * 100
+  print(f"{percent:.3f}%")
--- week2/problem-solving/난이도하_정수론_소수찾기_브론즈2.py
+++ week2/problem-solving/난이도하_정수론_소수찾기_브론즈2.py
@@ -1,2 +1,24 @@
 # 정수론 - 소수 찾기 (백준 브론즈2)
 # 문제 링크: https://www.acmicpc.net/problem/1978
+
+n = int(input())
+nums = map(int, input().split())
+
+
+import math
+
+result = 0
+for i in nums :
+  if i == 1 :
+    continue
+  elif i == 2 :
+    result += 1
+    continue
+  flag = True
+  for j in range(2, int(math.sqrt(i))+1) :
+    if i % j == 0 :
+      flag = False
+      break
+  if flag : result+=1
+    
+print(result)
