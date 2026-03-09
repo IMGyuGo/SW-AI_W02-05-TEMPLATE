ca9eb277e78ffbf767e98cf1f0180f42d1a06f50
IMGyuGo <gyugo4894@gmail.com>
2026-03-09 14:03:55 +0900

    [하] 문제 완성

--- week2/problem-solving/난이도하_문자열_단어공부_브론즈1.py
+++ week2/problem-solving/난이도하_문자열_단어공부_브론즈1.py
@@ -1,2 +1,31 @@
 # 문자열 - 단어 공부 (백준 브론즈1)
 # 문제 링크: https://www.acmicpc.net/problem/1157
+
+text = input()
+text = text.upper()
+textList = text[:]
+
+info = {}
+for i in range(len(textList)) :
+  if textList[i] in info.keys() :
+    info[textList[i]] += 1
+  else :
+    info[textList[i]] = 1
+
+max = max(info.values())
+
+result = ''
+dupli = 0
+for k in info.keys() :
+  if info[k] == max :
+    dupli+=1
+    result = k
+
+if dupli != 1 :
+  print('?')
+else :
+  print(result)
+
+# max값을 구하고
+# 중복값이 있으면 ?
+# 없으면 그 max 값의 위치
--- week2/problem-solving/난이도하_문자열_문자열반복_브론즈2.py
+++ week2/problem-solving/난이도하_문자열_문자열반복_브론즈2.py
@@ -1,2 +1,16 @@
 # 문자열 - 문자열 반복 (백준 브론즈2)
 # 문제 링크: https://www.acmicpc.net/problem/2675
+
+n = int(input())
+for _ in range(n) :
+  q = input().split()
+  iterNum = int(q[0])
+  text = q[1]
+
+  textList = text[:]
+
+  result = ""
+  for t in textList :
+    result += t*iterNum
+
+  print(result)
